8a31adc8087671cd38e4dfd02e8cb706ab56d92b
Ashish Gupta <ashish_29_dec@yahoo.com>
2014-04-28 18:55:38 -0700

    Added microphone image and changed the width and height of the widget

--- LauncherWidget/src/com/ashish/widget/launcher/LauncherApplication.java
+++ LauncherWidget/src/com/ashish/widget/launcher/LauncherApplication.java
@@ -5,20 +5,15 @@ import org.acra.annotation.ReportsCrashes;
 
 import android.app.Application;
 
-@ReportsCrashes(
-		formKey = "",
-		formUri = "http://www.mail.yahoo.com", 
-		formUriBasicAuthLogin = "ashish_29_dec",
-		formUriBasicAuthPassword = "FC30@lajpat"
-)
+
 public class LauncherApplication extends Application {
 
 	@Override
 	public void onCreate() {
 		// TODO Auto-generated method stub
 		super.onCreate();
-		
-		// The following line triggers the initialization of ACRA
-        ACRA.init(this);
+//		
+//		// The following line triggers the initialization of ACRA
+//        ACRA.init(this);
 	}
 }
